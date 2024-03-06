4521ae7252441d53d3550711cdd62fc6f4f1178c
sazzadursiam <sazzadur.swe@gmail.com>
2024-03-06 19:32:33 +0600

    tv media

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                // protocol: 'http',
-                // hostname: '127.0.0.1',
                 protocol: 'https',
                 hostname: 'api.rr-imperial.com',
 
