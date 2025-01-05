9b3e90ca8b959639b56be8bdc1397aaedae63340
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:21:05 -0700

    add metadata for Blogs page to enhance SEO and provide industry insights

---
+++ app/media/blogs/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "Blogs – RR Imperial Industry Insights and Updates",
+    description: "RR Imperial's latest blogs for valuable industry insights, cable solutions, and company updates. Stay informed about the latest trends, innovations, and news in the wire and cable industry.",
+    keywords:"RR Imperial Articles, RR Kabel Blog Posts, RR Kabel Insights"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
