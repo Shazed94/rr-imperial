8604e99f14c1cfacbbbf226aa2d2e7eaf83e07a6
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:53:09 -0700

    add metadata for RR Imperial Print Media section to enhance SEO and improve user engagement

---
+++ app/media/news/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export const metadata = {
+    title: "RR Imperial Print Media – Latest News and Publications",
+    description: "RR Imperial’s print media section for the latest articles, publications, and press coverage. Stay updated with our brand's achievements and industry contributions through various media outlets."
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
