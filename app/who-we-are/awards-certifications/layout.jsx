100efde26bb108630683b3b3f6824fbd1c6b6482
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 05:03:22 -0700

    add metadata for Awards & Certifications page to enhance SEO and showcase industry recognitions

---
+++ app/who-we-are/awards-certifications/layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Recognized Excellence: Awards & Certifications of RR Imperial",
+  description: "RR Imperial proudly showcases its industry awards and certifications, underscoring our commitment to superior quality and service. Discover more about our recognized achievements.",
+  keywords:"RR Imperial Industry Recognitions, RR Imperial Quality Certifications, Certification Achievements of RR Imperial"
+};
+  
+  export default function AwardsCertificationsLayout({ children }) {
+    return <>{children}</>;
+  }
