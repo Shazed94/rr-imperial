85248816bf81a4fa0a6b27d46107a8c05fa2926e
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:26:32 -0700

    add metadata for RR Imperial TV News Media section to enhance SEO and improve user engagement

---
+++ app/media/tv-media/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "RR Imperial TV News Media",
+    description: "Explore RR Imperial's TV News Media section for the latest broadcasts and features. Stay informed about our initiatives, awards, and industry insights through our television coverage.",
+    keywords:"RR Imperial TV News Media, RR Imperial Television Coverage, RR Imperial TV Features, RR Imperial Broadcast News"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
