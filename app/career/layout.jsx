b45320a36c19785e4b69072765e24d06038e6ba3
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 06:55:06 -0700

    add metadata for Career page to enhance SEO and promote job opportunities

---
+++ app/career/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const metadata = {
+    title: "Career",
+    description: "Explore rewarding career opportunities at RR Imperial. Submit your CV today to join a leading electrical manufacturing company dedicated to innovation and excellence. Shape your future with us.",
+    keywords:"Career Opportunities, Career Opportunities at RR Imperial, RR Imperial Job Openings, Join RR Imperial Team, RR Imperial Employment"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
