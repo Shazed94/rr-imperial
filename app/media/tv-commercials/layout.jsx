ab4b87ac639051b386d9dc0d5ae445bc9e0edaa7
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:24:22 -0700

    add metadata for RR Imperial TV Commercials page to enhance SEO and improve user engagement

---
+++ app/media/tv-commercials/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "RR Imperial TV Commercials – Watch Our Latest Ads",
+    description: "Explore RR Imperial's TV commercials and discover our brand’s latest advertisements. Learn about our cable solutions and innovative products through engaging TV ads. Watch now for more details!",
+    keywords:"RR Imperial TV Commercials, RR Imperial Advertisements, RR Kabel TV Ads, RR Imperial Broadcasts"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
