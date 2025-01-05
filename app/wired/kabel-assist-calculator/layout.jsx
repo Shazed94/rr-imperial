d467d7fe8e0f298c54890da683111150b1291dc8
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:08:05 -0700

    add metadata for Kabel Assist Calculator to enhance SEO and improve user engagement

---
+++ app/wired/kabel-assist-calculator/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "Kabel Assist Calculator | Accurate Cable Sizing Tool",
+    description: "Kabel Assist Calculator helps you select the right cable size for motors and electrical installations. Ensure safety and efficiency with precise calculations tailored to your needs. Explore now!",
+    keywords:"cable assist calculator, wire assist calculator, wire cable assist calculator, Cable Size Calculator, Electrical Cable Selector, Cable Assistance Tool"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
