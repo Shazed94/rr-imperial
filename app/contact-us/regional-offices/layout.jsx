33d012f9439a765affff891eaf9dde8d7f3735bb
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:14:47 -0700

    add metadata for Regional Offices page to enhance SEO and improve user support

---
+++ app/contact-us/regional-offices/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "Regional Offices",
+    description: "Find the regional offices of RR Imperial for quick assistance and support.",
+    keywords:"Regional Offices of RR Imperial, RR Imperial Locations, RR Imperial Contact Points, Regional Branches of RR Imperial"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
