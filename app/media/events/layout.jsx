7d243f8a8ebe33983dc1a1ef048486c2d64d09d3
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:54:54 -0700

    add metadata for RR Imperial Events page to enhance SEO and improve user engagement

---
+++ app/media/events/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "RR Imperial Events – Upcoming & Past Industry Gatherings",
+    description: "Discover the latest and past events hosted by RR Imperial. Stay updated on our industry exhibitions, conferences, and other key activities. Get involved with RR Kabel’s efforts to drive innovation in the cable manufacturing sector.",
+    keywords:"RR Kabel Events, RR Imperial Happenings, RR Kabel Activities"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
