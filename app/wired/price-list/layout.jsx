bf501538d611b8e681aa08bf7c7299ed9dca436e
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:04:13 -0700

    add metadata for RR Kabel price list page to enhance SEO and improve user access to pricing information

---
+++ app/wired/price-list/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "RR Kabel price list for wires and cables in bangladesh",
+    description: "Discover the latest RR Kabel price list for wires and cables in bangladesh. Access accurate pricing for top-quality electrical solutions. Download now to plan your next purchase with confidence.",
+    keywords:"rr kabel price list in bd, rr kable bd price list, rr kabel price list in bangladesh, rr kabel latest price list in bd, rr kabel latest price list in bangladesh, rr cable bangladesh price list, rr cable bd price list, rr cable latest price list in bd, rr cable latest price list in bangladesh, rr cable products price list in bd, rr kabel products price list in bd, rr kabel price list in bangladesh 2025, rr cable price list in bangladesh 2025, rr kabel price list in bangladesh 2025 pdf download, rr cable price list in bangladesh 2025 pdf download"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
