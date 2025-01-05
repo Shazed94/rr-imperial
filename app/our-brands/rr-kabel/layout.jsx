b777136214ee30727f4a54a967a4c572fea04200
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 05:46:11 -0700

    add metadata for RR Kabel page to enhance SEO and promote brand visibility

---
+++ app/our-brands/rr-kabel/layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: "RR Kabel: Pioneering Cable Solutions by RR Imperial",
+    description: "RR Kabel is manufactured in Bangladesh by RR Imperial Electricals Ltd, an ambitious ISO-certified sister concern of Imperial Group. RR Kabel is a pioneer in wire design, technology, and applicability.",
+    keywords:"RR Kabel, rr kabel bd, rr kabel brand in bangladesh, rr kabel products in bangladesh"
+  };
+  
+  export default function RRKabelLayout({ children }) {
+    return <>{children}</>;
+  }
