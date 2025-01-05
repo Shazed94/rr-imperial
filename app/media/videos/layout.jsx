68d74fe3d926007fcd2d352e7bac11dcedee069f
mstsurnalyakter <surnalyms@gmail.com>
2025-01-05 07:28:33 -0700

    add metadata for RR Imperial Videos page to enhance SEO and improve user engagement

---
+++ app/media/videos/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export const metadata = {
+    title: "RR Imperial Videos | Explore Media Content",
+    description: "RR Imperial's video library showcasing cables, solutions, and innovations. Watch media content to learn about RR Kabel’s offerings and industry insights. Stay informed with engaging visuals!",
+    keywords:"RR Kabel Media Content, RR Imperial Media Library, RR Kabel Promotional Videos"
+  };
+
+const layout = ({ children }) => {
+  return (
+    <>{ children }</>
+  )
+}
+
+export default layout
